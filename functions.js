81a6afaceff19e947600278697734f948f7284d8
TEParsons <todd.e.parsons@gmail.com>
2024-02-01 19:52:00 +0000

    enh: Set table headings from labels rather than hardcoding

--- functions.js
+++ functions.js
@@ -22,13 +22,14 @@ function hide_table() {
 
 /** Table building functions **/
 
+// array to represent workout
 const workout = {};
 
 function add_set() {
     // get relevant controls
-    let exercise_ctrl = document.getElementById("exercise");
-    let weight_ctrl = document.getElementById("weight");
-    let reps_ctrl = document.getElementById("reps");
+    let exercise_ctrl = document.getElementById("exercise-ctrl");
+    let weight_ctrl = document.getElementById("weight-ctrl");
+    let reps_ctrl = document.getElementById("reps-ctrl");
     // get exercise
     let key = exercise_ctrl.value;
     // if new exercise, add to workout
@@ -46,8 +47,11 @@ function add_set() {
 }
 
 function build_table() {
-    // get table element
+    // get elements
     let table_emt = document.getElementById("table");
+    let exercise_lbl = document.getElementById("exercise-lbl");
+    let weight_lbl = document.getElementById("weight-lbl");
+    let reps_lbl = document.getElementById("reps-lbl");
     // clear table
     table_emt.innerHTML = "";
     // make heading row
@@ -55,7 +59,7 @@ function build_table() {
     head_row.className = "grand-head"
     table_emt.appendChild(head_row);
     let exercise_head = document.createElement("th");
-    exercise_head.textContent = "Exercise";
+    exercise_head.textContent = exercise_lbl.textContent;
     head_row.appendChild(exercise_head);
     let w_r_head = document.createElement("th");
     head_row.appendChild(w_r_head);
@@ -82,10 +86,10 @@ function build_table() {
         weight_row.appendChild(exercise_th);
         // add weight / reps headings
         let weight_th = document.createElement("th");
-        weight_th.textContent = "Intensity";
+        weight_th.textContent = weight_lbl.textContent;
         weight_row.appendChild(weight_th);
         let reps_th = document.createElement("th");
-        reps_th.textContent = "Reps";
+        reps_th.textContent = reps_lbl.textContent;
         reps_row.appendChild(reps_th);
         // iterate through sets...
         for (let this_set of sets) {
@@ -101,7 +105,6 @@ function build_table() {
     }
     // add set headers
     for (let set_n of Array(cols).keys()) {
-        console.log(set_n)
         let set_head = document.createElement("th");
         set_head.textContent = `Set ${set_n + 1}`
         head_row.appendChild(set_head);
